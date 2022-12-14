825cf731e1896e98ce5c5ec740deeba54aaae196
SoumyaD27 <soumyad233@gmail.com>
2022-12-15 00:07:31 +0530

    [Dequeue]

---
+++ UC4_Dequeue/LinkedListQueue.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace UC4_Dequeue
+{
+    public class LinkedListQueue
+    {
+        Node head = null;
+
+        public void Enqueue(int data)
+        {
+
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+                Console.WriteLine("{0} inserted into Queue", node.data);
+            }
+        }
+        public void Dequeue(int data)
+        {
+
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+                Console.WriteLine("{0} deserted from Queue", node.data);
+            }
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ UC4_Dequeue/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UC4_Dequeue
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedListQueue linkedListQueue = new LinkedListQueue();
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(70);
+            linkedListQueue.Display();
+            linkedListQueue.Dequeue(56);
+            linkedListQueue.Dequeue(30);
+            linkedListQueue.Dequeue(70);
+          
+        }
+    }
+}
