f4916cbfdf9e2d3e6e97b63a8be3a4cc4794973c
SoumyaD27 <soumyad233@gmail.com>
2022-12-14 22:30:56 +0530

    [Stack using Peek and Pop]

---
+++ UC2_StackPeek AndPop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace UC2_StackPeek_AndPop
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            linkedListStack.Display();
+            linkedListStack.Peek();
+            linkedListStack.Pop();
+            linkedListStack.Display();
+            
+
+        }
+    }
+}
