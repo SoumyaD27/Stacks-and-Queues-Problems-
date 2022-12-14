d6e4b1e0081e29793b54aab8412a52b81e228dff
SoumyaD27 <soumyad233@gmail.com>
2022-12-14 23:51:07 +0530

    [Create Queue]

---
+++ UC3_CreateQueue/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace UC3_CreateQueue
+{
+    public class Program
+    {
+        public static void Main(string[] args) 
+        {
+            LinkedListQueue linkedListQueue = new LinkedListQueue();
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(70);
+            linkedListQueue.Display();
+        }
+    }
+}
