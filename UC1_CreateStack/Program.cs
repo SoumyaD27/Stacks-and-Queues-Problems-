857f5c435fe7ad544323611694abbe98e87afd8e
SoumyaD27 <soumyad233@gmail.com>
2022-12-14 16:29:53 +0530

    [Create a Stack]

---
+++ UC1_CreateStack/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace UC1_CreateStack
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(56);
+            linkedListStack.Push(30);
+            linkedListStack.Push(70);
+            linkedListStack.Display();
+
+        }
+    }
+}
